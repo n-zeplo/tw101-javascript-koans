8d4982a7dd282da47bd8fceeb8866a665df41200
nzeplo <nzeplowitz@gmail.com>
2015-05-02 20:57:06 -0500

    Add Solution to Problem 1

--- js/problem1.js
+++ js/problem1.js
@@ -28,7 +28,17 @@ function kiteGame(numberOfKites) {
     changeElementText("#numberOfKites", numberOfKites);
     var answer = "";
 
-    // write some code here!
+    for(var i = 1;i<=numberOfKites;i++){
+        if( i % 3 === 0 && i % 5 === 0 ){
+            answer += "Peche! Ipo kaate! ";
+        } else if ( i % 3 === 0){
+            answer += "Peche! ";
+        } else if ( i % 5 === 0){
+            answer += "Ipo kaate! ";
+        } else {
+            answer += i + " ";
+        }
+    }
 
     changeElementText("#answer", answer);
 }
